8a938e5c73055e85ea6dd2078be39a05f2d58d7c
Clarisse Kessab <clarisseksb@gmail.com>
2025-01-20 15:31:10 +0100

    Creation du model Room avec attributs + 1er Constructor

--- src/main/java/com/clarissek/airbnb_clone/model/Room.java
+++ src/main/java/com/clarissek/airbnb_clone/model/Room.java
@@ -1,5 +1,19 @@
 package com.clarissek.airbnb_clone.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+  private Long id;
+  private String roomType;
+  private BigDecimal roomPrice;
+  private boolean isBooking = false;
+  private List<BookingRoom> bookings;
+
+  public Room() {
+    this.bookings = new ArrayList<>();
+  }
+
 
 }
