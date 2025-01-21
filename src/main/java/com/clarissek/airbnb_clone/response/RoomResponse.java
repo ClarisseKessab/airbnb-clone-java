1a3d59d6ff87d9c10c3b8262dbe5b21c800d6d6c
Clarisse Kessab <clarisseksb@gmail.com>
2025-01-21 13:34:00 +0100

    Initialize Room Response

--- src/main/java/com/clarissek/airbnb_clone/response/RoomResponse.java
+++ src/main/java/com/clarissek/airbnb_clone/response/RoomResponse.java
@@ -1,5 +1,49 @@
 package com.clarissek.airbnb_clone.response;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.Base64;
+import java.util.List;
+
+import com.clarissek.airbnb_clone.model.BookingRoom;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Base64;
+
+
+@Data
+@NoArgsConstructor
 public class RoomResponse {
+  private Long id;
+  private String roomType;
+  private BigDecimal roomPrice;
+  private boolean isBooking = false;
+  private String photo;
+  private List<BookingRoomResponse> bookings;
+
+  public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+    this.id = id;
+    this.roomType = roomType;
+    this.roomPrice = roomPrice;
+  }
+
+  public RoomResponse(Long id, String roomType, BigDecimal roomPrice, boolean isBooking, byte[] photoBytes,
+      List<BookingRoomResponse> bookings) {
+    this.id = id;
+    this.roomType = roomType;
+    this.roomPrice = roomPrice;
+    this.isBooking = isBooking;
+    this.photo = (photoBytes != null) ? Base64.getEncoder().encodeToString(photoBytes) : null;
+    this.bookings = bookings;
+  }
+
+
+
 
 }
